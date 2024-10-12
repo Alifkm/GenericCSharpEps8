908d159cceaa850a5e7886c4f10cdb8e1eedc8bb
Alifkm <alifkahfi@gmail.com>
2024-10-12 21:10:38 +0700

    feat: first commit

---
+++ GenericCSharp_eps8/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GenericCSharp_eps8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var productOne = new Product();
+            var productTwo = new Product();
+
+            bool isCompare = Comparer.AreEqual(productOne, productTwo);
+            Console.WriteLine(isCompare); 
+
+        }
+    }
+
+    class Product
+    {
+
+    }
+}
